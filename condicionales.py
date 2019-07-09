3dfebf9fa100f7549e1fb59004b5b7f9aa5f4a53
labsol <labsol@nn1.innovalabsnet.net>
2019-07-09 12:34:48 -0500

    condiciones

---
+++ condicionales.py
@@ -0,0 +1,31 @@
+x = 10
+y = 5
+
+
+if x > y:
+	print('(0) es mayor que (1)'.format(x,y))
+else:
+	print('(1) es mayor (0) '.format(x,y))
+
+x=5
+y=10
+
+
+if x>y:
+	print (x)
+elif x==y:
+	print('son iguales')
+else:
+	print(y)
+if x != y:
+	print('diferentes')
+elif y>x:
+	print('(0) es mayor'.format(x))
+else:
+	print (x)
+
+if y>2 and y<=10:
+	print('(0) es mayor que dos y menor o igual a 10'.format(y))
+
+if x<y or x>10:
+	print('(0)'.format(x))
